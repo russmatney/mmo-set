cf6ab300ad9c49a03ec4c6c006c3ab15cdd18d82
Russell Matney <russell.matney@gmail.com>
2013-12-07 19:44:50 -0500

    removes component stuff.

--- index.js
+++
@@ -1,9 +0,0 @@
-
-var express = require('express');
-var app = module.exports = express();
-
-app.set('view engine', 'jade');
-
-app.use(express.static(__dirname + '/public'));
-
-app.use(require('boot'));
