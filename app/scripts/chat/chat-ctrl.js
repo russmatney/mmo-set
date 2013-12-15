bc266db98f3ff5b14c7a200e4eaea1d0ed1cf906
Russell Matney <russell.matney@gmail.com>
2013-12-14 16:43:43 -0800

    basic chat client

--- app/scripts/chat/chat-ctrl.js
+++ app/scripts/chat/chat-ctrl.js
@@ -5,16 +5,16 @@ angular.module('chat', [])
     $scope.messages = [];
     
     socket.on('send:message', function(message){
-      $scope.messages.push(message);
+      $scope.messages.push({message: message, time: new Date()});
     });
     
     socket.on('user:join', function (data) {
-      $scope.messages.push("New user joined!" + data);
+      $scope.messages.push({message: "New User Joined!", time: new Date()});
     });
 
     $scope.sendMessage = function () {
         socket.emit('send:message', $scope.message);
-        $scope.messages.push($scope.message);
+        $scope.messages.push({message: $scope.message, time: new Date()});
         $scope.message = '';
     };
     
